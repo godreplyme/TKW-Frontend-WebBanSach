3d13104b2c27db1a433a3552e26ccd5fff14c966
godreplyme <nguyenhovu0984@gmail.com>
2023-08-14 23:00:31 +0700

    update json

--- js/fetch.js
+++ js/fetch.js
@@ -1,21 +1,24 @@
-window.onload=function(){
-let productContainer=document.querySelector(".product-content")
-fetch("js/storage.json").then(res=>res.json()).then(data=>{
-    for(let d of data)
-    {
-        console.log('hello')
-        productContainer.insertAdjacentHTML("beforeend",`
-        <div class="product">
-            <img src="${d.image}">
-            <h2 class="name">${d.name}</h2>
-            <div class="writer"><a>${d.writer}</a></div>
-            <div class="price">${d.price}&#8363;</div>
-            <ul class="prices">
-                <li><span class="raw-price">${d.rawprice}&#8363;</span></li>
-                <li><span class="offer">-${d.offer}%</span></li>
-            </ul>
-        </div>
-        `)
-    }
-    })
-}
+function sachGiaoKhoa(){
+    let productContainer=document.querySelector(".product-content")
+    productContainer.innerHTML = '';
+        fetch("js/storage.json").then(res=>res.json()).then(data=>{
+            for(let d of data)
+            {
+                if(d.type1=="sachgk"){
+                productContainer.insertAdjacentHTML("beforeend",`
+                <div class="product">
+                    <img src="${d.image}">
+                    <h2 class="name" title=${d.name}>${d.name}</h2>
+                    <div class="writer"><a>${d.writer}</a></div>
+                    <div class="price">${d.price}&#8363;</div>
+                    <ul class="prices">
+                        <li><span class="raw-price">${d.rawprice}&#8363;</span></li>
+                        <li><span class="offer">-${d.offer}%</span></li>
+                    </ul>
+                </div>
+                `)}
+            }
+            })
+        }
+
+
